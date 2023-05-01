06a73da806153d73e788573104fd91e7cfde49b7
KimJunHo98 <dydgnscc6845@gmail.com>
2023-05-01 18:11:33 +0900

    230501 spec 컴포넌트 aos애니메이션 추가

--- src/components/main/Spec.js
+++ src/components/main/Spec.js
@@ -14,16 +14,30 @@ const Spec = () => {
                                 </div>
                             </div>
                             <div className="my_education">
-                                <h2 className="education">Education</h2>
-                                <div className="academy1">
+                                <h2 className="education"
+                                    data-aos="fade-down"
+                                    data-aos-duration="1000"
+                                >
+                                    Education
+                                </h2>
+                                <div className="academy1"
+                                    data-aos="fade-down"
+                                    data-aos-duration="2000"
+                                >
                                     <p className="tit">그린컴퓨터아카데미 프로젝트기반 프론트엔드 (React,Vue) 웹&앱 SW개발자 양성과정 수료</p>
                                     <span className="date">2022.11 ~ 2023.05</span>
                                 </div>
-                                <div className="academy2">
+                                <div className="academy2"
+                                    data-aos="fade-down"
+                                    data-aos-duration="2000"
+                                >
                                     <p className="tit">두드림컴퓨터아카데미 UI&UX 반응형 웹 디자인&웹 퍼블리셔 양성과정 수료</p>
                                     <span className="date">2022.05 ~ 2022.09</span>
                                 </div>
-                                <div className="univ">
+                                <div className="univ"
+                                    data-aos="fade-down"
+                                    data-aos-duration="2000"
+                                >
                                     <p className="tit">수원과학대학교 건축학과 졸업</p>
                                     <span className="date">2017.03 ~ 2022.02</span>
                                 </div>
