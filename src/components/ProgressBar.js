400c955c639c58e7bdeaa05cf2a59a9ed1fe6c9f
KimJunHo98 <dydgnscc6845@gmail.com>
2023-06-20 19:51:34 +0900

    230620 lodash 라이브러리 설치, ProgressBar 컴포넌트 스크롤이벤트 쓰로틀링 적용

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { throttle } from 'lodash';
 
 const ProgressBar = () => {
     const [width, setWidth] = useState(0);
 
     useEffect(() => {
-        const updateProgress = () => {
-            const scrollY = window.scrollY || window.pageYOffset;
+        const updateProgress = throttle(() => {
+            console.log('Throttled scroll event'); 
+            const scrollY = window.pageYOffset || document.documentElement.scrollTop || window.scrollY;
             const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
             const progress = (scrollY / pageHeight) * 100;
 
             setWidth(progress);
-        };
-
+        }, 200);
+        
         window.addEventListener('scroll', updateProgress);
 
         return () => {
