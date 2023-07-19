7dc3fb4194c98c6c53eea0d8f88f8649069548d4
KimJunHo98 <dydgnscc6845@gmail.com>
2023-07-19 18:36:55 +0900

    230719 프로젝트 버튼 링크 수정

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -20,13 +20,13 @@ const Projects = () => {
             url3: "https://github.com/KimJunHo98/project1/blob/main/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B81%20%EA%B8%B0%ED%9A%8D%EC%84%9C.pdf",
         },
         {
-            url1: "https://bbatsue-web.vercel.app/",
-            url2: "https://github.com/hejo47/bbatsueWeb",
+            url1: "https://anbd.vercel.app/",
+            url2: "https://github.com/2ANBD/ANBD",
             // url3: "file:///C:/Users/Administrator/Downloads/%ED%8C%80-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8_0203%EC%A0%9C%EC%B6%9C.pdf",
         },
         {
-            url1: "https://anbd.vercel.app/",
-            url2: "https://github.com/2ANBD/ANBD",
+            url1: "https://bbatsue-web.vercel.app/",
+            url2: "https://github.com/hejo47/bbatsueWeb",
             // url3: "file:///C:/Users/Administrator/Downloads/%ED%8C%80-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8_0203%EC%A0%9C%EC%B6%9C.pdf",
         },
         {
@@ -118,8 +118,8 @@ const Projects = () => {
                                                 <p>구&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;현:<span>메인 페이지, 서브 페이지 5개 / filter함수와 map함수를 활용하여 검색기능 구현, 상품업로드 실시간으로 반영 / 상품등록, 결제기능 구현 / gitflow방식으로 브랜치를 나눠서 협업 / 웹&앱으로 구현</span></p>
                                             </div>
                                             <div className="site_btns">
-                                            <button className="site_link_btn" onClick={()=>{window.open(btnLink[2].url1)}}><FontAwesomeIcon icon={faLink} /><span>{btnName[0].name}</span></button>
-                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[2].url2)}}><FontAwesomeIcon icon={faGithub} /><span>{btnName[1].name}</span></button>
+                                            <button className="site_link_btn" onClick={()=>{window.open(btnLink[1].url1)}}><FontAwesomeIcon icon={faLink} /><span>{btnName[0].name}</span></button>
+                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[1].url2)}}><FontAwesomeIcon icon={faGithub} /><span>{btnName[1].name}</span></button>
                                                 {/* <button className="site_link_btn" onClick={()=>{window.open(btnLink[2].url3)}}><FontAwesomeIcon icon={faFilePdf} /><span>{btnName[2].name}</span></button> */}
                                             </div>
                                         </div>
@@ -139,8 +139,8 @@ const Projects = () => {
                                                 <p>구&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;현:<span>메인 페이지, 서브 페이지 4개 / 영양정보 API 사용 / 로컬스토리지를 활용한 로그인 기능 구현 / gitflow방식으로 브랜치를 나눠서 협업</span></p>
                                             </div>
                                             <div className="site_btns">
-                                            <button className="site_link_btn" onClick={()=>{window.open(btnLink[1].url1)}}><FontAwesomeIcon icon={faLink} /><span>{btnName[0].name}</span></button>
-                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[1].url2)}}><FontAwesomeIcon icon={faGithub} /><span>{btnName[1].name}</span></button>
+                                            <button className="site_link_btn" onClick={()=>{window.open(btnLink[2].url1)}}><FontAwesomeIcon icon={faLink} /><span>{btnName[0].name}</span></button>
+                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[2].url2)}}><FontAwesomeIcon icon={faGithub} /><span>{btnName[1].name}</span></button>
                                                 {/* <button className="site_link_btn" onClick={()=>{window.open(btnLink[1].url3)}}><FontAwesomeIcon icon={faFilePdf} /><span>{btnName[2].name}</span></button> */}
                                             </div>
                                         </div>
@@ -160,8 +160,8 @@ const Projects = () => {
                                                 <p>구&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;현:<span>메인 페이지, 서브 페이지 2개 / firebase를 사용한 CRUD기능 구현</span></p>
                                             </div>
                                             <div className="site_btns">
-                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[0].url1)}}><FontAwesomeIcon icon={faLink} /><span>Site</span></button>
-                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[0].url2)}}><FontAwesomeIcon icon={faGithub} /><span>Github</span></button>
+                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[3].url1)}}><FontAwesomeIcon icon={faLink} /><span>Site</span></button>
+                                                <button className="site_link_btn" onClick={()=>{window.open(btnLink[3].url2)}}><FontAwesomeIcon icon={faGithub} /><span>Github</span></button>
                                             </div>
                                         </div>
                                         <div className="hor_item_bottom">
